dd9096d15942a3e1a0d92069b838443990aa603e
lfj19a <81330813+lfj19a@users.noreply.github.com>
2021-10-05 21:29:28 -0400

    Added Workouts table

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-#Jason Graham JG19M
+#Jason Graham JG19M && Lance Fairbanks LJF19a
 #The program in this file is the individual work of Jason Graham and group members
 
 
 import sqlite3
 
-conn = sqlite3.connect('reviewData.db')
+conn = sqlite3.connect('Gainz.db')
 print ("Opened database successfully")
 
 ##sets up two tables in the database
 conn.execute('CREATE TABLE login (Username TEXT, Name TEXT)')
+conn.execute('CREATE TABLE Workouts (Name TEXT, Type TEXT, Exercises TEXT)')
 print ("Login Table created successfully")
 
 conn.close()
