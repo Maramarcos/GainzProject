4ebcc38540aeffcb511d01dd83130c418493149e
lfj19a <81330813+lfj19a@users.noreply.github.com>
2021-11-01 21:50:20 -0400

    App Traversal (Need to fix registration)

--- app.py
+++ app.py
@@ -3,20 +3,64 @@
 
 from flask import Flask, render_template, request
 import sqlite3 as sql
-from datetime import datetime
 app = Flask(__name__)
 
-#HomePage
+# HTML PAGES
 @app.route('/')
-def home():
-	return render_template('home.html')
+def homepage():
+    return render_template('index.html')
+
+#login to website
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return render_template('workouts.html')
+    return render_template('login.html', error=error)
+
+@app.route('/hub')
+def hub():
+    return render_template('hub.html')
 
 #Takes the user to the register html page so they can input their account information
 @app.route('/register')
 def register():
 	return render_template('register.html')
 
-#Performes the action of adding a new user to the database
+@app.route('/workouts')
+def workouts():
+    return render_template('workouts.html')
+
+@app.route('/addWorkout')
+def addWorkout():
+    return render_template('addWorkout.html')
+
+@app.route('/editWorkout')
+def editWorkout():
+    return render_template('editWorkout.html')
+
+@app.route('/completeWorkout')
+def completeWorkout():
+    return render_template('completeWorkout.html')
+
+
+# Forms and Lists
+@app.route('/addWorkoutForm')
+def addWorkoutForm():
+    return render_template('workouts.html')
+
+@app.route('/editWorkoutForm')
+def editWorkoutForm():
+    return render_template('workouts.html')
+
+@app.route('/completeWorkoutList')
+def completeWorkoutList():
+    return render_template('completeWorkoutModule.html')
+
+#Takes the user to the register html page so they can input their account information
 @app.route('/registration',methods = ['POST', 'GET'])
 def registeration():
 	if request.method == 'POST':
@@ -29,7 +73,7 @@ def registeration():
 			add = request.form['address:']
 			ema = request.form['email:']
 			pas = request.form['pass:']
-			
+
 			with sql.connect("Gainz.db") as con:
 				cur = con.cursor()
 				cur.execute("INSERT INTO login (username, firstname,lastname,gender,phone,address,email,password) VALUES (?,?,?,?,?,?,?,?)",(use,fir,las,gen,pho,add,ema,pas))
@@ -41,28 +85,6 @@ def registeration():
 		finally:
 			return render_template("login.html")
 			con.close()
-	
-#login to website
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return render_template('workouts.html')
-    return render_template('login.html', error=error)
 
-		
-#Go to workouts
-@app.route('/workouts')
-def workouts():
-	return render_template('workouts.html')
-    
-#Create a workout
-@app.route('/addWorkout')
-def addWorkout():
-	return render_template('addWorkout.html')
-    
 if __name__ == '__main__':
 	app.run(host='0.0.0.0');
