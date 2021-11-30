0a4b840abad2d2f4471fdbe4f7a04f5cfc3c9582
lfj19a <81330813+lfj19a@users.noreply.github.com>
2021-11-30 03:46:47 -0500

    Working on workout functionality

--- flask_app/server/app/routes.py
+++ flask_app/server/app/routes.py
@@ -1,13 +1,16 @@
 from app import app, db
-from flask import render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user, login_user, logout_user
 from app.forms import RegistrationForm, LoginForm, WorkoutForm
 from app.models import User
+import sqlite3 as sql
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if current_user.is_authenticated:
-        return render_template('base/index.html')
+            workouts = ['       ', 'taco', 'pizza']
+            progress = ['       ', 'taco', 'pizza']
+            return render_template('base/index.html', workouts = workouts, progress = progress)
     return render_template('base/home.html')
 
 @app.route('/register', methods=["GET", "POST"])
@@ -28,6 +31,7 @@ def healthtips():
 @app.route('/credits')
 def credits():
     return render_template('info/credits.html')
+
 
 @app.route("/logout", methods=["GET"])
 @login_required
@@ -57,16 +61,76 @@ def login():
             return redirect(url_for("index"))
     return render_template('user/login.html', form=form)
 
-@app.route('/workout/new',  methods=['GET', 'POST'])
+# This is for dropdown of workouts
+
+@app.route('/workoutsSelect', methods=['POST'])
+def workoutsSelect():
+    if request.method == 'POST':
+        return render_template('base/index.html')
+
+
+# This is for dropdown of progress reports
+
+@app.route('/progressSelect', methods=['POST'])
+def progressSelect():
+    if request.method == 'POST':
+        return render_template('base/index.html')
+
+
+# This is for creating a workout
+@app.route('/workout/addWorkout',  methods=['GET', 'POST'])
+def addWorkout():
+    return render_template('workout/addWorkout.html')
+
+
+@app.route('/workout/addWorkoutNow',  methods=['GET', 'POST'])
 @login_required
-def new_workout():
+def addWorkoutNow():
+    if request.method == 'POST':
+        try:
+            title = request.form['title']
+            setTime = request.form['workInterval']
+            restTime = request.form['restInterval']
+            W1 = request.form['W1']
+            W2 = request.form['W2']
+            W3 = request.form['W3']
+            W4 = request.form['W4']
+            W5 = request.form['W5']
+            W6 = request.form['W6']
+            W7 = request.form['W7']
+            W8 = request.form['W8']
+            W9 = request.form['W9']
+            W10 = request.form['W10']
+
+            with sql.connect("app.db") as con:
+                cur = con.cursor()
+
+                cur.execute("INSERT INTO workouts(title,setTime,RestTime,W1,W2,W3,W4,W5,W6,W7,W8,W9,W10) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",(title,setTime,restTime,W1,W2,W3,W4,W5,W6,W7,W8,W9,W10) )
+
+                con.commit()
+                msg = "Records successfully added!"
+
+        except:
+            con.rollback()
+            msg = "Error in insertion!"
+
+        finally:
+            return render_template('base/index.html')
+            con.close()
+
+# This is for creating a report
+
+@app.route('/workout/addReport',  methods=['GET', 'POST'])
+@login_required
+def addReport():
     '''Create a new workout.'''
     form = WorkoutForm()
-    return render_template("workout/new_workout.html", form=form)
+    return render_template("workout/addReport.html", form=form)
 
-@app.route('/workout/<workout_id>')
+# This is for running a workout
+
+@app.route('/workout/<workout_title>')
 @login_required
-def workout(workout_id, methods=['GET']):
-    '''Display workout by id.'''
+def workout(workout_title, methods=['GET']):
+    '''Display workout by title.'''
     return ""
-
